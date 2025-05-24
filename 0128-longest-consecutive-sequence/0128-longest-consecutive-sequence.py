a244f68f492f6c1f8ae4efdc907f89f261519277
Ray Laciste <67614706+RayLaciste@users.noreply.github.com>
2025-05-23 20:34:28 -0700

    0128-LONGEST-CONSECUTIVE-SEQUENCE Stats: Time: 58 ms (36.69%), Space: 34.4 MB (19.52%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,21 +1,33 @@
+# class Solution:
+#     def longestConsecutive(self, nums: List[int]) -> int:
+        # if len(nums) == 0:
+        #     return 0
+        # elif len(nums) == 1:
+        #     return 1
+
+        # seq_length=1
+        # max_length=1
+        # s_nums = sorted(set(nums))
+        # print(s_nums)
+        # for i in range(len(s_nums)-1):
+        #     if s_nums[i] + 1 == s_nums[i+1]:
+        #         seq_length+=1
+        #     else:
+        #         if seq_length>max_length:
+        #             max_length=seq_length
+        #         seq_length=1
+        #     if seq_length>max_length:
+        #             max_length=seq_length
+        # return max_length
+        # ^ My original solution
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if len(nums) == 0:
-            return 0
-        elif len(nums) == 1:
-            return 1
-
-        seq_length=1
-        max_length=1
-        s_nums = sorted(set(nums))
-        print(s_nums)
-        for i in range(len(s_nums)-1):
-            if s_nums[i] + 1 == s_nums[i+1]:
-                seq_length+=1
-            else:
-                if seq_length>max_length:
-                    max_length=seq_length
-                seq_length=1
-            if seq_length>max_length:
-                    max_length=seq_length
-        return max_length
+        num_set = set(nums)
+        longest = 0
+        for n in num_set:
+            if n - 1 not in num_set:
+                length = 1
+                while n + length in num_set:
+                    length += 1    
+                longest = max(longest, length)
+        return longest
