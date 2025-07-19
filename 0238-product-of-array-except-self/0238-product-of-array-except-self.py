ce933d571d57c40be9f1f49f2c18356de2aa4c9c
Ray Laciste <67614706+RayLaciste@users.noreply.github.com>
2025-07-18 17:00:39 -0700

    0238-PRODUCT-OF-ARRAY-EXCEPT-SELF Stats: Time: 39 ms (17.2%), Space: 27 MB (11.57%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,17 +1,29 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        ans=[1] * len(nums) # initialize answer list        
+        # res = [0] * len(nums)
         
-        prefix = 1
+        # for i in range(len(nums)):
+        #     product = 1
+        #     for j in range(len(nums)):
+        #         if i == j:
+        #             continue
+        #         product = product * nums[j]
+
+        #     res[i] = product
+        
+        # return res
+
+        res = [0] * len(nums)
+        before = [1] * len(nums)
+        after = [1] * len(nums)
+
+        for i in range(1, len(nums)):
+            before[i] = nums[i - 1] * before[i - 1]
+
+        for i in range(len(nums) - 2, -1, -1):
+            after[i] = nums[i + 1] * after[i + 1]
+        
         for i in range(len(nums)):
-            ans[i] = prefix
-            prefix *= nums[i]
-            """
-            [1, 2, 4, 6] -> ans[1,1,2,8]
-            """
-        #postfix
-        postfix=1
-        for i in range(len(nums) - 1, -1, -1):
-            ans[i] *= postfix
-            postfix *= nums[i]
-        return(ans)
+            res[i] = before[i] * after[i]
+        
+        return res
